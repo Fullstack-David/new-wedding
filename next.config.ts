9807da16227e1a33c62da09f5975d7dfce7c31bd
David Heidari <davidheidari2@gmail.com>
2026-04-17 16:48:05 +0200

    'fixed'

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { start } from "repl";
 
 const nextConfig: NextConfig = {
   // output: "export",
-  next: start,
+  // next: start,
   trailingSlash: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
